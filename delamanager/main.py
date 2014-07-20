9bce6239070cb2733d0a6725752d634fdd5e1515
Thomas Levine <_@thomaslevine.com>
2014-07-20 19:32:03 -0400

    -

--- delamanager/main.py
+++ delamanager/main.py
@@ -5,17 +5,19 @@ from db import Dadabase
 db = Dadabase('sqlite:////home/tlevine/foo.db')
 b = Bottle()
 
-@b.post('/v1/')
+@b.post('/directions')
 def directions():
     before = request.remote_route
-    if db.at_limit(before):
-        response.status_code = 429
-    else:
+    if db.under_limit(before):
+        response.status_code = 200
         return json.dumps({
             'before_address': before,
             'file_number': db.file_number(),
         })
+    else:
+        response.status_code = 429
 
+@b.post('/response')
 def response():
-    return json.dumps({
-    })
+    after = request.remote_route
+    db.
