3239bac1a735174f43f5dbed0d45eb6958f0ef8c
Thomas Levine <_@thomaslevine.com>
2014-07-19 20:07:08 -0400

    -

--- deleware/main.py
+++ deleware/main.py
@@ -1,7 +1,9 @@
 
-def main():
-    home_response   = home()
-    for i in range(1):
+def look(firm_file_numbers):
+    'Given a firm file number, try to get the information.'
+    home_response = home()
+    for firm_file_number in firm_file_numbers:
         search_response = search(home_response, firm_file_number)
-        result_response = result(search_response, firm_file_number):
+        if True:
+            result_response = result(search_response, firm_file_number):
     
