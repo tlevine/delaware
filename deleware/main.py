b6668dbfbd426d5fef755431475338d74cd27d15
Thomas Levine <_@thomaslevine.com>
2014-07-19 20:02:02 -0400

    main

---
+++ deleware/main.py
@@ -0,0 +1,7 @@
+
+def main():
+    home_response   = home()
+    for i in range(1):
+        search_response = search(home_response, firm_file_number)
+        result_response = result(search_response, firm_file_number):
+    
