f1c713bc16345bf4a065db71cb7f1302dbd61a92
Thomas Levine <_@thomaslevine.com>
2014-07-20 07:14:50 -0400

    communicate with server

--- worker/direction.py
+++ worker/direction.py
@@ -9,12 +9,17 @@ def auth(manager_address, username, installation):
     return username, password
 
 def respond(manager_address, username, installation, before_address):
-    return requests.post(manager_address + '/respond',
-                         auth = auth(manager_address, username, installation),
-                         data = {'before_address': before_address})
+    r = requests.post(manager_address + '/response',
+                      auth = auth(manager_address, username, installation),
+                      data = {'before_address': before_address})
+    return r
 
 def directions(manager_address, username, installation):
-    return requests.post(manager_address + '/direct
+    r = requests.post(manager_address + '/directions',
+                      auth = auth(manager_address, username, installation))
+    if r.ok:
+        data = json.loads(r.text)
+        return data['file_number'], data['before_address']
 
 def sleep():
     seconds = sum(random.randint(0,1) for _ in range(100)) / 10
