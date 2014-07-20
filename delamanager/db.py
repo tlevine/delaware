9d40b5d6c7cc50091d29ff385280a39f73d8d04e
Thomas Levine <_@thomaslevine.com>
2014-07-20 18:32:05 -0400

    -

--- delamanager/db.py
+++ delamanager/db.py
@@ -1,6 +1,20 @@
-DB = None
+from collections import Counter
 
+import dataset
 
-def at_limit(ip_address):
-    params = []
-    blah = DB.execute('SELECT count(*) FROM requests WHERE datetime > ? AND ip_address > ?', params)
+class Dadabase:
+    def __init__(self, url):
+        self.disk = dataset.connect(url)
+        self.file_numbers = {file_number:0 for file_number in range(1, 8 * 10**6)}
+        sql = 'SELECT file_number, count(*) FROM file_numbers GROUP BY file_number'
+        for row in self.disk.query(sql):
+            self.file_numbers[row['file_number']] = row['count(*)']
+    def query(self, *args, **kwargs):
+        return self.disk.query(*args, **kwargs)
+    def increment_file_number(self, file_number):
+        self.disk.insert({'file_number':file_number})
+        self.file_numbers['file_number'] += 1
+
+db = Dadabase('sqlite:////home/tlevine/foo.db')
+
+    blah = db.query('SELECT count(*) FROM requests WHERE datetime > ? AND ip_address > ?', params)
