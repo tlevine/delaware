0840f2e06f58e7a8c60bbcf197aa0fd4382e9dc4
Thomas Levine <_@thomaslevine.com>
2014-07-20 04:20:17 -0400

    -

--- worker/main.py
+++ worker/main.py
@@ -1,9 +1,34 @@
+from functools import partial
 
-def look(firm_file_numbers):
+import worker.download as dl
+import worker.direction as d
+
+def main():
+    manager_address, username, installation = params()
+    respond = partial(d.respond, manager_address, username, installation)
+    directions = partial(d.directions, manager_address, username, installation)
+    sleep = d.sleep
+    while True:
+        firm_file_number, before_address = get_work(directions, sleep)
+        do_work(firm_file_number, partial(respond, before_address))
+
+def params():
+    raise NotImplementedError
+
+def get_work(directions, sleep):
+    while True:
+        answer = directions()
+        if answer == None:
+            sleep()
+        else:
+            return answer
+
+def do_work(firm_file_number, respond):
     'Given a firm file number, try to get the information.'
-    home_response = home()
+    home_response = dl.home()
+    respond(home_response)
     for firm_file_number in firm_file_numbers:
-        search_response = search(home_response, firm_file_number)
-        if True:
-            result_response = result(search_response, firm_file_number):
-    
+        search_response = dl.search(home_response, firm_file_number)
+        respond(search_response)
+        result_response = dl.result(search_response, firm_file_number):
+        respond(result_response)
