6e8b9e5c3eb68a7863a321eb6c31aa364665ad19
Thomas Levine <_@thomaslevine.com>
2014-07-20 07:28:24 -0400

    use no basic auth

--- delaworker/direction.py
+++ delaworker/direction.py
@@ -4,19 +4,24 @@ from hashlib import sha1
 
 import requests
 
-def auth(manager_address, username, installation):
-    password = sha1(manager_address.encode('utf-8') + installation.encode('utf-8')).hexdigest()
-    return username, password
+def salt(username, installation):
+    return sha1(manager_address.encode('utf-8') + installation.encode('utf-8')).hexdigest()
 
 def respond(manager_address, username, installation, before_address):
-    r = requests.post(manager_address + '/response',
-                      auth = auth(manager_address, username, installation),
-                      data = {'before_address': before_address})
+    data = {
+        'username': username,
+        'salted_installation': salt(username, installation),
+        'before_address': before_address,
+    }
+    r = requests.post(manager_address + '/response', data = data)
     return r
 
 def directions(manager_address, username, installation):
-    r = requests.post(manager_address + '/directions',
-                      auth = auth(manager_address, username, installation))
+    data = {
+        'username': username,
+        'salted_installation': salt(username, installation),
+    }
+    r = requests.post(manager_address + '/directions', data = data)
     if r.ok:
         data = json.loads(r.text)
         return data['file_number'], data['before_address']
