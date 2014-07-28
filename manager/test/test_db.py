6cd3d20489074b4f07d06f235c5febc2300263a8
Thomas Levine <_@thomaslevine.com>
2014-07-28 11:42:34 +0000

    start db tests

--- manager/test/test_db.py
+++ manager/test/test_db.py
@@ -1,13 +1,36 @@
 from unittest import TestCase
 
+from db import Dadabase
+
 class TestDadabase(TestCase):
+    def setUp(self):
+        self.db = Dadabase(self, dburl, requestdir):
+
     def test_init(self):
         pass
     def test_file_number(self):
-        pass
+        'The file number should be among those with the lowest counts.'
+        self.db.file_numbers = {
+            1: 4,
+            9: 2,
+            2: 3,
+            12: 2,
+        }
+        n.assert_in({9,12}, self.db.file_number())
+
     def test_under_limit(self):
         pass
+
     def test_increment_file_number(self):
-        pass
+        file_number = 82342
+
+        self.db.disk['file_numbers'] # and select stuff
+        self.db.file_numbers[file_number]
+
+        self.db.increment_file_number(file_number)
+
+        self.db.disk['file_numbers'] # and select stuff
+        self.db.file_numbers[file_number]
+
     def test_save_request(self):
         pass
