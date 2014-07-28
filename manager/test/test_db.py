e1c34c6f3171846c87488fae4b3a288e074aa149
Thomas Levine <_@thomaslevine.com>
2014-07-28 11:33:27 +0000

    stubs for db test

---
+++ manager/test/test_db.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+class TestDadabase(TestCase):
+    def test_init(self):
+        pass
+    def test_file_number(self):
+        pass
+    def test_under_limit(self):
+        pass
+    def test_increment_file_number(self):
+        pass
+    def test_save_request(self):
+        pass
