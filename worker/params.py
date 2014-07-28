ab2a35085f11fe846e1de7cacc278e5d274cba75
Thomas Levine <_@thomaslevine.com>
2014-07-28 09:03:38 +0000

    configparser params tests

--- worker/params.py
+++ worker/params.py
@@ -1,17 +1,23 @@
 import configparser
+import os
 import uuid
+from getpass import getuser
 
 SECTION = 'worker'
-CONFIG = os.path.expanduser(os.path.join('~', '.deleware', 'config')
+DIRECTORY = os.path.expanduser(os.path.join('~', '.delaware'))
+CONFIG = os.path.join(DIRECTORY, 'config')
 
 def params():
-    from_config = read_config_params()
+    from_config = read_config_params(CONFIG)
     if from_config == None:
         manager_address, username = prompt_params()
         installation = installation_id()
-        write_config_params(manager_address, username, installation, open(CONFIG, 'w'))
+        if not os.path.exists(DIRECTORY):
+            os.makedirs(DIRECTORY)
+        write_config_params(manager_address, username, installation, CONFIG)
     else:
         manager_address, username, installation = from_config
+
     return manager_address, username, installation
 
 def installation_id():
@@ -22,23 +28,30 @@ def prompt_params():
     manager_address = input('Manager [%s]: ' % default_manager_address)
     if manager_address == '':
         manager_address = default_manager_address
-    username = input('Username: ')
+
+    default_username = getuser()
+    username = input('Username [%s]: ' % default_username)
+    if username == '':
+        username = default_username
+
     return manager_address, username
 
-def read_config_params():
+def read_config_params(filename):
     c = configparser.ConfigParser()
-    c.read(CONFIG)
+    c.read(filename)
 
     manager_address = c.get(SECTION, 'manager_address', fallback = None)
     username = c.get(SECTION, 'username', fallback = None)
     installation = c.get(SECTION, 'installation', fallback = None)
 
-    if manager_address != None and username != None and installation != None):
+    if manager_address != None and username != None and installation != None:
         return manager_address, username, installation
 
-def write_config_params(manager_address, username, installation, fp):
+def write_config_params(manager_address, username, installation, filename):
     c = configparser.ConfigParser()
+    c.add_section(SECTION)
     c.set(SECTION, 'manager_address', value = manager_address)
     c.set(SECTION, 'username', value = username)
-    c.set(SECTION, 'installation', installation = installation)
-    c.write(fp)
+    c.set(SECTION, 'installation', value = installation)
+    with open(filename, 'w') as fp:
+        c.write(fp)
---
+++ worker/test/test_params.py
@@ -0,0 +1,24 @@
+import os
+import tempfile
+from shutil import copyfile
+
+import nose.tools as n
+
+import worker.params as p
+
+CONFIG = os.path.join('worker','test','fixtures','config')
+
+def test_installation_id():
+    observed = p.installation_id()
+    n.assert_equal(len(observed), 36)
+
+def test_read_config_params():
+    tmp = tempfile.NamedTemporaryFile()
+    copyfile(CONFIG, tmp.name)
+    observed = p.read_config_params(tmp.name)
+    expected = ('https://delaware.dada.pink', 'tlevine',
+                '065db5d3-924c-4129-9c93-2360538a4ce5')
+    n.assert_tuple_equal(observed, expected)
+
+def test_write_config_params():
+    pass
