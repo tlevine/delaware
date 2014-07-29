0acac34b7c1bc044c7bb66ed9facf874edcf41fc
Thomas Levine <_@thomaslevine.com>
2014-07-29 18:02:02 +0000

    logfile

--- manager/args.py
+++ manager/args.py
@@ -5,3 +5,5 @@ parser.add_argument('--database', default = 'sqlite:////home/delaware/delaware.d
     help = 'SQLAlchemy-style relational database URL')
 parser.add_argument('--request-directory', default = '/home/delaware/requests',
     help = 'Directory wherein data from workers will be stored')
+parser.add_argument('--logfile', default = '/home/delaware/delaware.log',
+    help = 'File to which a summary of each request will be written')
