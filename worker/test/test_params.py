3bedd71503e018633f348c578e199538b699590c
Thomas Levine <_@thomaslevine.com>
2014-07-29 10:20:15 +0000

    fix test

--- worker/test/test_params.py
+++ worker/test/test_params.py
@@ -17,13 +17,15 @@ def test_read_config_params():
     tmp = tempfile.NamedTemporaryFile()
     copyfile(CONFIG, tmp.name)
     observed = p.read_config_params(tmp.name)
-    expected = ('https://delaware.dada.pink', 'tlevine',
+    expected = ('the-ca-bundle-file.crt',
+                'https://delaware.dada.pink', 'tlevine',
                 '065db5d3-924c-4129-9c93-2360538a4ce5')
     n.assert_tuple_equal(observed, expected)
 
 def test_write_config_params():
     tmp = tempfile.NamedTemporaryFile()
     copyfile(CONFIG, tmp.name)
-    p.write_config_params('https://delaware.dada.pink', 'tlevine',
+    p.write_config_params('the-ca-bundle-file.crt',
+                          'https://delaware.dada.pink', 'tlevine',
                           '065db5d3-924c-4129-9c93-2360538a4ce5', tmp.name)
     n.assert_true(filecmp.cmp(tmp.name, CONFIG))
