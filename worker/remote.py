7ea5a686f553b0485bada4b2277015e96511f5eb
Thomas Levine <_@thomaslevine.com>
2014-07-29 18:41:57 +0000

    json

--- worker/remote.py
+++ worker/remote.py
@@ -23,7 +23,7 @@ def respond(verify, to_dict, manager_address, username, installation, before_add
         'finished': finished,
         'response': to_dict(response)
     }
-    r = requests.post(manager_address + '/response', data = data, verify = verify)
+    r = post(manager_address + '/response', data = data)
     logger.info('Uploaded a %07d response' % file_number)
     return r.json()['ip_address']
 
@@ -34,8 +34,14 @@ def directions(verify, manager_address, username, installation):
         'username': username,
         'salted_installation': salt(username, installation),
     }
-    r = requests.post(manager_address + '/directions', data = data, verify = verify)
+    r = post(manager_address + '/directions', data)
     if r.ok:
         data = r.json()
         logger.info('Received directions to query %07d' % data['file_number'])
         return data['file_number'], data['ip_address']
+
+HEADERS = {
+    'content-type': 'application/json',
+}
+def post(url, data):
+    return requests.post(url, data = json.dumps(data), verify = verify, headers = HEADERS)
