0b5c97999741304b097801d99f6f5a633e461127
Thomas Levine <_@thomaslevine.com>
2014-07-31 11:13:18 +0000

    satisfied with my plot

--- docs/choosing-rate-limits.R
+++ docs/choosing-rate-limits.R
@@ -2,13 +2,16 @@ N <- 8e6
 ip.addresses <- 10 * 2^(3:6)
 daily.quota <- 100 * 2^(1:4)
 
-plot(0, 0, main = 'What should the daily quota be?',
-     xlab = 'Daily request quota',
-     xlim = c(0, max(daily.quota)),
-     ylab = 'How many days to download all the data',
-     ylim = c(0, 2*N/(min(daily.quota) * min(ip.addresses))),
-     type = 'n', bty = 'n'
+plot(0, 0, main = 'What should the rate limit be?',
+     xlab = 'Number of requests that one worker makes per day',
+     xlim = c(0, max(daily.quota) + 100),
+     ylab = 'How many months to download all the data',
+     ylim = c(0, 400),
+     type = 'n', bty = 'n', axes = F
 )
+axis(1)
+axis(2, at = ((0:12)/12) * 365, labels = 0:12, las = 1)
+abline(h = 0)
 
 color <- 1
 for (i in ip.addresses) {
@@ -16,7 +19,7 @@ for (i in ip.addresses) {
     lines(y ~ daily.quota, col = color)
     color <- color + 1
 }
-text(min(daily.quota), 2*N/(ip.addresses * min(daily.quota)),
-     label = ip.addresses, pos = 2)
-text(max(daily.quota)/2, 2*N/(min(ip.addresses)*min(daily.quota)),
-     'Different lines are for different\namounts of IP addresses.\n(20 addresses, 40 addresses, &c.)', pos = 1)
+text(max(daily.quota), 2*N/(ip.addresses * max(daily.quota)),
+     label = ip.addresses, pos = 4)
+text(max(daily.quota), 350, pos = 2,
+     'Different lines are for different\namounts of IP addresses.\n(80 addresses, 160 addresses, &c.)')
